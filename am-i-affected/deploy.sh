5f857deea4bdefbb102cc70f0d7a88f9cca0a723
Tomasz Drwięga <tomusdrw@gmail.com>
2017-11-08 13:07:17 +0100

    Deploy script.

---
+++ am-i-affected/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -xe
+
+yarn install
+yarn build
+rm -rf ../build
+mv build ../
+cd ..
+git stash
+git checkout gh-pages
+rm -rf static
+mv build/* .
+rmdir build
+git add .
+git ca -m "Update"
+git push
+git checkout master
+cd am-i-affected
+git stash pop
